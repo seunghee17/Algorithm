261bee5449d4ecbd3969072901757807c25b134d
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-02-10 12:44:23 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 120 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.time.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+		
+			if(a==0&&b==0) break;
+			System.out.println(a+b);
+		}
+		
+		br.close();
+	}
+	
+}
