7a8f6314befe78ef53ca1913122921988217ed69
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-06-24 12:50:57 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 16532 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -4,66 +4,57 @@ import java.util.*;
 import java.math.*;
 
 public class Main {
-	static int[] calcul;
-	static int N;
-	static int[] A;
-	static int min=Integer.MAX_VALUE;
-	static int max=Integer.MIN_VALUE;
-	static ArrayList<Integer> list = new ArrayList<>();
+	static int n;
+	static int min = Integer.MAX_VALUE;
+	static int max = Integer.MIN_VALUE;
+	static int[] arr;
+	static int[] cal;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		A = new int[N];
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for(int i=0; i<N; i++) {
-			//순서를 바꾸면 안되는 후보 숫자들 
-			A[i] = Integer.parseInt(st.nextToken());
+		for(int i=0; i<n; i++) {
+			if(st.hasMoreTokens()) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
 		}
-		calcul = new int[4];
+		cal = new int[4];
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i=0; i<4; i++) {
-			calcul[i] = Integer.parseInt(st.nextToken());
+			cal[i] = Integer.parseInt(st.nextToken());
 		}
-		dfs(0,A[0]);
-		
+		dfs(0, arr[0]);
 		System.out.println(max);
 		System.out.println(min);
 	}
-	static void dfs(int depth, int result) {
-		if(depth == N-1) {
-			min = Math.min(min, result);
-			max = Math.max(max, result);
+	
+	static void dfs(int idx, int sum) {
+		if(idx == n-1) {
+			max = Math.max(max, sum);
+			min = Math.min(min, sum);
 			return;
 		}
-		depth++;
+		idx++;
 		for(int i=0; i<4; i++) {
-			
-			if(calcul[i] >0) {
-				calcul[i]--;
+			if(cal[i] >0 ) {
+				cal[i]--;
 				switch(i) {
 				case 0: 
-					dfs(depth, result+A[depth]);
+					dfs(idx, sum + arr[idx]);
 					break;
-				case 1: 
-					dfs(depth, result-A[depth]);
+				case 1: 					
+					dfs(idx, sum - arr[idx]);
 					break;
-				case 2: 
-					dfs(depth, result*A[depth]);
+				case 2: 					
+					dfs(idx, sum * arr[idx]);					
 					break;
-				case 3:
-//					if(result<0) {
-//						result = (-1*result) / A[depth];
-//						dfs(depth, result);
-//					} else {
-//						dfs(depth, result/=A[depth]);
-//					}
-					dfs(depth, result/A[depth]);
-				break;
+				case 3: 					
+					dfs(idx, sum / arr[idx]);
+					break;
 				}
-				calcul[i]++;
+				cal[i]++;	
 			}
 		}
-		
 	}
-	
 }
