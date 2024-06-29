64195e4ec9da6a19a40fc81f178dcd9bda762eed
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-06-29 16:02:26 +0900

    [Silver II] Title: 꽃길, Time: 172 ms, Memory: 18368 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.java
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -1,95 +1,66 @@
-
 import java.io.*;
 import java.util.*;
 import java.math.*;
 
+
 public class Main {
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {-1,1,0,0};
-	static int n=0;
-	static int min = Integer.MAX_VALUE;
-	static boolean[][] visited;
 	static int[][] graph;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		graph = new int[n][n];
-		visited = new boolean[n][n];
-		//boolean[][] visited = new boolean[n][n];
-		//화단의 각 영역 가격 받음 
-		for(int i=0; i<n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for(int j=0; j<n; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		dfs(0,0);
-		System.out.println(min);
-		
- 	}
-	static void dfs(int flower, int sum) {
-		if(flower == 3) {
-			min = Math.min(min, sum);
-			return;
-		}
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				//visited배열과 범위 내부에 있는지 확인해야한다 
-				if(isvalid(i,j)) {
-					setting(i,j,true);
-					dfs(flower + 1 , sum + calcul(i,j));
-					setting(i,j,false);
-				}
-			}
-		}
-		
-	}
-	
-	static boolean isvalid(int a, int b) {
-		if(visited[a][b]) {
-			return false;
-		} else {
-			//꽃잎을 검사하기 위하여 
-			for(int i=0; i<4; i++) {
-				int nx = a+dx[i];
-				int ny = b+dy[i];
-				if(nx>=0 && nx < n && ny>=0 && ny<n) {
-					if(visited[nx][ny] == true) {
-						//이미 씨앗이든 꽃이 있음 
-						return false;					
-					}
-				}else return false;
-			}
-		}
-		return true;
-	}
-	
-	static void setting(int a, int b, boolean bool) {
-		if(bool) {
-			visited[a][b] = true;
-			for(int i=0; i<4; i++) {
-				int nx = a+dx[i];
-				int ny = b+dy[i];
-				visited[nx][ny] = true;
-			}
-		} else {
-			visited[a][b] = false;
-			for(int i=0; i<4; i++) {
-				int nx = a+dx[i];
-				int ny = b+dy[i];
-				visited[nx][ny] = false;
-			}
-		}
-	}
-	static int calcul(int a, int b) {
-		int sum = graph[a][b];
-		for(int i=0; i<4; i++) {
-			int nx = a+dx[i];
-			int ny = b+dy[i];
-			sum += graph[nx][ny];
-		}
-		return sum;
-	}
-		
+	static boolean[][] visited;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static int n;
+	static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	n = Integer.parseInt(br.readLine());
+    	graph = new int[n][n];
+    	visited = new boolean[n][n];
+    	for(int i=0; i<n; i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+    		for(int j=0; j<n; j++) {
+    			graph[i][j] = Integer.parseInt(st.nextToken());
+    		}
+    	}
+    	solution(0,0);
+    	System.out.println(min);
+    	
+    }
+   
+    static void solution(int flower, int sum) {
+    	if(flower == 3) {
+    		min = Math.min(sum, min);
+    		return;
+    	} 
+    	for(int i=0; i<n; i++) {
+    		for(int j=0; j<n; j++) {
+    			if(!visited[i][j] && isvalid(i,j)) {
+    				int cost = graph[i][j];
+    				visited[i][j] = true;
+    				
+    				for(int k=0; k<4; k++) {
+    					int nx = i+dx[k];
+    					int ny = j+dy[k];
+    					visited[nx][ny] = true;
+    					cost += graph[nx][ny];
+    				}
+    				solution(flower+1, sum+cost);
+    				visited[i][j] = false;
+    				for(int k=0; k<4; k++) {
+    					int nx = i+dx[k];
+    					int ny = j+dy[k];
+    					visited[nx][ny] = false;
+    				} 
+    			}
+    		}
+    	}
+    }
+    static boolean isvalid(int x, int y) {
+    	for(int i=0; i<4; i++) {
+    		int nx = x+dx[i];
+    		int ny = y + dy[i];
+    		if(nx>=0 && nx < n && ny>=0 && ny<n && !visited[nx][ny]) continue;
+    		else return false; //범위 초과하거나 누군가 visit했거나 
+    	}
+    	return true;
+    }
 }
