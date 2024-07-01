b8cb541e296e6ab7462c263673cc34970ee52496
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-07-01 11:17:23 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -2,24 +2,26 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long[] D = new long[1001];
-		D[1] = 1;
-		D[2] = 2;
-		for(int i=3; i<=n; i++) {
-			D[i] = -1;
-		}
-		
-		System.out.println(topdown(n, D));
-	}
-	static long topdown(int v, long[] D) {
-		if(D[v] != -1) {
-			return D[v];
-		}
-		return D[v] = (topdown(v-1, D) + topdown(v-2, D)) % 10007;
-	}
 
+public class Main {
+	static long[] arr;
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int n = Integer.parseInt(br.readLine());
+    	arr = new long[1001];
+    	arr[1] = 1;
+    	arr[2] = 2;
+    	for(int i=3; i<=n; i++) {
+    		arr[i] = -1;
+    	}
+    	
+    	System.out.println(recur(n));
+    }
+   static long recur(int n) {
+	   if(arr[n] != -1) {
+		   return arr[n];
+	   }
+	   return arr[n] = (recur(n-2) +recur(n-1)) % 10007;
+   }
+   
 }
