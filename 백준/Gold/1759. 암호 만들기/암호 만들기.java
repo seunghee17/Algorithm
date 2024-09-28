5417b3284fe774a7de5649b786312d41a2c2da98
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-09-28 18:42:05 +0900

    [Gold V] Title: 암호 만들기, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,55 +2,65 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
-
 public class Main {
-	static char[] arr,result;
-	static boolean[] visited;
-	static int l,c;
-	static StringBuilder sb  = new StringBuilder();
+	static int l;
+	static int c;
+	static List<Character> hubo = new ArrayList<Character>();
+	static List<Character> chogun = new ArrayList<Character>();
+	static Character[] dap;
+	static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
     	l = Integer.parseInt(st.nextToken());
     	c = Integer.parseInt(st.nextToken());
-    	arr = new char[c];
-    	result = new char[l];
-    	visited = new boolean[c];
-    	String s = br.readLine().replace(" ", "");
+    	dap= new Character[c];
+    	String s = br.readLine();
+    	s = s.replace(" ", ""); 
     	for(int i=0; i<c; i++) {
-    		arr[i] = s.charAt(i);
-    	}
-    	Arrays.sort(arr);
-    	solution(0,0);
-    	System.out.print(sb);
-    }
-    static void solution(int depth, int start) {
-    	if(depth == l) {
-    		int a =0;
-    		int b=0;
-    		for(int i=0; i<l; i++) {
-    			if(result[i] == 'a' || result[i] == 'e' || result[i] == 'i' || result[i] == 'o' || result[i] == 'u') {
-    				a++;
-    			} else b++;
-    		}
-    		if(a>=1 && b>=2) {
-    			for(int i=0; i<l; i++) {
-    				sb.append(result[i]);
-    			}
-    			sb.append('\n');
-    		}
-    		return;
+    		hubo.add(s.charAt(i));
     	}
     	
-    	for(int i=start; i<c; i++) {
-    		if(!visited[i]) {
-    			visited[i] = true;
-    			result[depth] = arr[i];
-    			solution(depth+1, i+1);
-    			visited[i] = false;
+    	Collections.sort(hubo);
+    	chogun = Arrays.asList('a', 'e', 'i', 'o', 'u');
+    	calcul(0,0);
+    	System.out.println(sb);
+    }
+    
+    static void calcul(int idx, int depth) {
+    	if(depth==l && isvalid(dap)) {
+    		for(int i=0; i<l; i++) {
+    			sb.append(dap[i].toString());
     		}
+    		sb.append('\n');
+    		return;
+    	}
+    	if(idx>c-1) return;
+    	for(int i = idx; i<c; i++) {
+    		dap[depth] = hubo.get(i);
+    		calcul(i+1, depth+1);
+    	
     	}
     }
+    
+    static boolean isvalid(Character[] dap) {
+    	int a=0;
+    	int b=0;
+    	
+    	for(int i=0; i<l; i++) {
+    		//모음이다 
+    		if(chogun.contains(dap[i])) {
+    			a++;
+    		} else {
+    			b++;
+    		}
+    	}
+    	if(a>=1 && b>=2) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
 
-  
 }
