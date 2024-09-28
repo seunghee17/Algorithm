7fde9dc0dfd7d75d39812c286d230c96cb852115
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-09-28 16:28:50 +0900

    [Silver III] Title: N과 M (1), Time: 216 ms, Memory: 22096 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,38 +3,40 @@ import java.io.*;
 import java.util.*;
 import java.math.*;
 
-
 public class Main {
-	static boolean[] visited;
-	static int[] arr;
-	static int n,m;
+	static int n;
+	static int m;
+	static boolean visit[];
+	static int arr[];
+	static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
     	n = Integer.parseInt(st.nextToken());
     	m = Integer.parseInt(st.nextToken());
-    	visited = new boolean[n];
-    	arr = new int[m]; //수열을 출력하는 용도 
-    	find(0);
+    	visit = new boolean[n+1];
+    	arr = new int[n+1];
+    	go(0);
+    	System.out.println(sb);
     }
     
-    static void find(int depth) {
-    	if(depth ==m) {
-    		for(int i: arr) {
-    			System.out.print(i + " ");
+    public static void go(int index) {
+    	if(index == m) {
+    		for(int i=0; i<m; i++) {
+    			sb.append(arr[i]).append(" ");
     		}
-    		System.out.println();
+    		sb.append('\n');
     		return;
     	}
-    	for(int i=0; i<n; i++) {
-    		if(visited[i] == false) {
-    			visited[i] = true;
-    			arr[depth] = i+1;
-    			find(depth+1);
-    			visited[i] = false;
+    	for(int i=1; i<=n; i++) {
+    		if(!visit[i]) {
+    			visit[i] = true;
+    			arr[index] = i;
+    			go(index+1);
+    			visit[i] = false;
     		}
     	}
-    	return;
     }
-  
+
 }
+    
