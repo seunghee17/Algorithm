f07ccaa8f0ec98a379aab0b50f4274805879f41c
seunghee song <80136506+seunghee17@users.noreply.github.com>
2024-10-27 07:51:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.74 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,24 +1,20 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int m;
-    static int n;
+    static int[][] visited;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    static int[][] visited;
-    public int solution(int[][] maps) {
-        m = maps.length; //행
-        n = maps[0].length; //열
-        visited = new int[m][n];
-       int answer = game(maps);
-        if(answer == 0) {
+    public int solution(int[][] maps) { //최소 칸의 수 리턴
+        int n = maps.length;
+        int m = maps[0].length;
+        visited = new int[n][m];
+        if(bfs(n,m,maps) == 0) {
             return -1;
         } else {
-            return answer;
+            return bfs(n,m,maps);
         }
     }
-    static int game(int[][] maps) {
+    static int bfs(int n, int m, int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[] {0,0});
         visited[0][0] = 1;
@@ -27,16 +23,14 @@ class Solution {
             int nx = now[0];
             int ny = now[1];
             for(int i=0; i<4; i++) {
-                int newx = nx+dx[i];
+                int newx = nx + dx[i];
                 int newy = ny + dy[i];
-                if(newx>=0 && newx<m && newy>=0 && newy<n) {
-                    if(maps[newx][newy]==1 && visited[newx][newy]==0) {
-                        visited[newx][newy] = visited[nx][ny] + 1;
-                        q.offer(new int[] {newx, newy});
-                    }
+                if(newx>=0 && newx<n && newy>=0 && newy<m && visited[newx][newy] ==0 && maps[newx][newy] !=0) {
+                    visited[newx][newy] = visited[nx][ny] +1;
+                    q.offer(new int[] {newx, newy});
                 }
             }
         }
-        return visited[m-1][n-1];
+        return visited[n-1][m-1];
     }
 }
